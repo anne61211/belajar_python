dfd75072bbcae29137b098998356e09cd9cc953d
anne61211 <annelelonoo@gmail.com>
2025-08-07 16:26:44 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from p5 import *
+from random import randint
+
+def mouse_pressed():
+    print("🎯")
+    if hit_colour == Color("blue").hex:
+        print("You hit the outer circle, 50 points!")
+    elif hit_colour == Color("red").hex:
+        print("You hit the inner circle, 200 points!")
+    elif hit_colour ==  Color("yellow").hex:
+        print("You hit the middle, 500 points!")
+    else:
+        print("You missed! No points!")
+
+
+def shoot_arrow():
+    global hit_colour
+    arrow_x = randint(100,300)
+    arrow_y = randint(100,300)
+    hit_colour = get(arrow_x, arrow_y).hex
+    # print(hit_colour)
+    fill("brown")
+    circle(arrow_x, arrow_y, 15)
+    
+def setup():
+    size(400,400)
+    no_stroke()
+    
+def draw():
+    fill("cyan")
+    rect(0,0,400,250)
+    fill("lightgreen")
+    rect (0,250,400,150)
+    fill("brown")
+    triangle(150,350,200,150,250,350)
+    fill("blue")
+    circle(200,200,170)
+    fill("red")
+    circle(200,200,110)
+    fill("yellow")
+    circle(200,200,30)
+    shoot_arrow()
+
+run(frame_rate=2)
